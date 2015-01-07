a5d8a68e40e3e2156a21c050cd46d4e450cde0b9
= <=>
2015-01-07 18:01:17 -0500

    Created dummy blog posts

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,10 +2,29 @@
 
 angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    this.personal = 'personal';
+
+    var post = {
+      content : 'blah blah blah I am a test post.'
+    };
+
+    var blog1 = {
+      name : 'blog1',
+      author : 'John Doe',
+      posts : [ post, post, post ]
+    };
+
+    var blog2 = {
+      name : 'blog2',
+      author : 'Jane Doe',
+      posts : [ post, post, post ]
+    };
+
+    var blog3 = {
+      name : 'blog3',
+      author : 'Jimmy Dong',
+      posts : [ post, post, post ]
+    };
+    console.log(blog1);
+    $scope.blogs = [ blog1, blog2, blog3 ];
+    this.count = 3;
   });
