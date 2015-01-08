15c512f6751cd9f5b178b59b5d506a48e49c9ccc
Clement Hoang <clement.hoang24@gmail.com>
2015-01-08 14:39:42 -0500

    Post view completed

--- client/app/scripts/controllers/post.controller.js
+++ client/app/scripts/controllers/post.controller.js
@@ -1,9 +1,23 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('PostCtrl', ['$stateParams', function ($stateParams) {
+  .controller('PostCtrl', ['$stateParams', '$rootScope', function ($stateParams, $rootScope) {
+
+    var getArrayOfMatchingPosts = function(titleToMatch) {
+      var matches = $rootScope.blogs[$stateParams.blogName].posts.filter( function (postInBlog) {
+        return postInBlog.title === titleToMatch;
+      });
+
+      return matches;
+    };
 
     this.postTitle = $stateParams.postTitle;
 
+    if (getArrayOfMatchingPosts(this.postTitle).length !== 1) {
+      throw new Error('Unexpected array length.');
+    }
+
+    this.post =  getArrayOfMatchingPosts(this.postTitle)[0];
+    this.author = $rootScope.blogs[$stateParams.blogName].author;
   }]);
 
